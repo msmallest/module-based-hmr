cacdf81f9ecb9b2c3e5baf03324bcbd84c64e61a
Michael Small <michael.small@relationshipone.com>
2025-01-20 20:05:30 -0600

    chore: document that inline template/style also causes issue

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Run `ng-serve` (HMR enabled by default)
 5. Persists until refresh
 
 Three things of note
-1. These errors happen when saving an external template or external style. TODO - check inline ones next commit
+1. These errors happen when saving an file-based template or file-based style, or saving an inline template or inline style. Did not check a combo of one being file-based and one being. The errors looked the same in either scenario, but in particular were sourced in both file-based.
 2. These errors are on the first save. However, if you add some more characters and keep saving, the links to the source files no longer go to the source files and instead go to the `@ng` output files
 3. When these errors happen (either the kind with source files directly or `@ng` files, the content in the `*ngFor/async` go away.)
 
--- src/app/authorized-users/authorized-users.component.ts
+++ src/app/authorized-users/authorized-users.component.ts
@@ -5,11 +5,26 @@ import { map, Observable, of, startWith } from 'rxjs';
 @Component({
   selector: 'app-authorized-users',
   standalone: false,
-
   templateUrl: './authorized-users.component.html',
-  styleUrl: './authorized-users.component.css'
+  styleUrl: './authorized-users.component.css',
+  // template: `
+  //   <p>Error with ngFor</p>
+  //   <ng-container *ngFor="let option of options">
+  //     <div>{{ option }}</div>
+  //   </ng-container>
+
+  //   <p>Error with async pipe</p>
+  //   <ng-container *ngFor="let option of options$ | async">
+  //     <div>{{ option }}</div>
+  //   </ng-container>
+  // `,
+  // styles: `
+  //   p {
+  //     color: red;
+  //   }
+  // `,
 })
 export class AuthorizedUsersComponent {
-  options$ = of([1, 2, 3])
-  options = [1,2,3]
+  options$ = of([1, 2, 3]);
+  options = [1, 2, 3];
 }
